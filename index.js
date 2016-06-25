dd26d8866c67ac67d4cd32d7ad44e9bfd11fd6f3
Michael Wang <michaelzwang92@gmail.com>
2016-06-24 22:28:59 -0700

    find location

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 $(function(){
 
   $('input[type="submit"]').click(function(){
-    console.log($('#zip').val())
+    var zip = $('#zip').val();
+    var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&APPID=';
+
+    $.getJSON(weatherAPI, function(response){
+      var weather_data = response;
+
+      $('.location').html("Location: " + weather_data["name"])
+    });
   });
 
 });
