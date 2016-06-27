7c5023366cbfce921a52bca4651af7fd81b189ba
Michael Wang <michaelzwang92@gmail.com>
2016-06-27 12:02:36 -0700

    display weather icons for single and multiple weather descriptions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 $(function(){
 
   $('input[type="submit"]').click(function(){
+    $('.icons').html('')
     var zip = $('#zip').val();
     var weatherAPI = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip+',us&APPID=' + API_KEY;
 
@@ -10,18 +11,39 @@ $(function(){
 
       if (weather_data["weather"].length === 1){
         var description = weather_data["weather"][0]["description"];
+        var icon = weather_data["weather"][0]["icon"];
+        var icon_url = "http://openweathermap.org/img/w/" + icon + ".png"
+        $('.icons').prepend('<img class="weather-icon" src=' + icon_url + ' />')
       }
       else {
         var description = "";
+        var icons = []
+        
         weather_data["weather"].forEach(get_descriptions);
         function get_descriptions(item) {
           description = description + item["description"] + " "
-        }; 
+        };
+        
+        weather_data["weather"].forEach(get_icons); 
+        function get_icons(item) {
+          icons.push(item["icon"])
+        };
+
+        var icon_urls = icons.map(function(icon) {
+          var icon_url = "http://openweathermap.org/img/w/" + icon + ".png";
+          return icon_url;
+        });
+
+        icon_urls.forEach(add_icon);
+        function add_icon(url) {
+          $('.icons').append('<img class="weather-icon" src=' + url + ' />')
+        };
       }
 
       $('.location').html(weather_data["name"]);
       $('.description').html(description);
-      $('.temperature').html(temperature_far)
+      $('.temperature').html(temperature_far);
+
     });
   });
 
